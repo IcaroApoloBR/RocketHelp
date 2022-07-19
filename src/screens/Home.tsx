ad6fc2d2f979882f25fbb5b2c73e0f1cfb3a4f0e
Icaro Apolo <apoloraci@gmail.com>
2022-07-19 20:09:51 -0300

    feat(navigation): useNavigation stack, create routes

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList, Center } from 'native-base';
 import { SignOut } from 'phosphor-react-native';
 import { ChatTeardropText } from 'phosphor-react-native';
@@ -11,43 +12,23 @@ import { Order, OrderProps } from '../components/Order';
 export function Home() {
     const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
     const [orders, setOrders] = useState<OrderProps[]>([
-        // {
-        //     id: '123',
-        //     patrimony: '123456',
-        //     when: '18/07/2022 às 18:00',
-        //     status: 'open'
-        // },
-        // {
-        //     id: '1234',
-        //     patrimony: '123456',
-        //     when: '18/07/2022 às 18:00',
-        //     status: 'closed'
-        // },
-        // {
-        //     id: '12345',
-        //     patrimony: '123456',
-        //     when: '18/07/2022 às 18:00',
-        //     status: 'open'
-        // }, {
-        //     id: '123456',
-        //     patrimony: '123456',
-        //     when: '18/07/2022 às 18:00',
-        //     status: 'open'
-        // },
-        // {
-        //     id: '1234567',
-        //     patrimony: '123456',
-        //     when: '18/07/2022 às 18:00',
-        //     status: 'closed'
-        // },
-        // {
-        //     id: '12345678',
-        //     patrimony: '123456',
-        //     when: '18/07/2022 às 18:00',
-        //     status: 'open'
-        // }
+        {
+            id: '123',
+            patrimony: '123',
+            when: '123',
+            status: 'open',
+        }
     ]);
+    const navigation = useNavigation();
     const { colors } = useTheme();
+
+    function handleNewOrder() {
+        navigation.navigate('new');
+    }
+
+    function handleOpenDetails(orderId: string) {
+        navigation.navigate('details', { orderId })
+    }
 
     return (
         <VStack flex={1} pb={6} bg="gray.700">
@@ -99,7 +80,7 @@ export function Home() {
                 <FlatList
                     data={orders}
                     keyExtractor={item => item.id}
-                    renderItem={({ item }) => <Order data={item} />}
+                    renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)} />}
                     showsVerticalScrollIndicator={false}
                     contentContainerStyle={{ paddingBottom: 100 }}
                     ListEmptyComponent={() => (
@@ -113,7 +94,7 @@ export function Home() {
                     )}
                 />
 
-                <Button title="Nova solicitação" />
+                <Button title="Nova solicitação" onPress={handleNewOrder} />
             </VStack>
 
         </VStack>
